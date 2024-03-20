aec45a83c13940da06ceb28f497fb958b1f25ec9
gilteixeira <gilteixeira@ua.pt>
2024-03-20 22:42:04 +0000

    Expo 48 example working

--- App.tsx
+++ App.tsx
@@ -1,7 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Client, Message } from "react-native-paho-mqtt";
+
+//Set up an in-memory alternative to global localStorage
+const myStorage: any = {
+  setItem: (key: string, item: string) => {
+    myStorage[key] = item;
+  },
+  getItem: (key: string) => myStorage[key],
+  removeItem: (key: string) => {
+    delete myStorage[key];
+  },
+};
 
 export default function App() {
+  useEffect(() => {
+    // Create a client instance
+    const client = new Client({
+      uri: "ws://bearkillerpt.xyz:8082/",
+      clientId: "GoodTester",
+      storage: myStorage,
+    });
+
+    // set event handlers
+    client.on(
+      "connectionLost",
+      (responseObject: { errorCode: number; errorMessage: string }) => {
+        if (responseObject.errorCode !== 0) {
+          console.log(responseObject.errorMessage);
+        }
+      }
+    );
+    client.on("messageReceived", (message: any) => {
+      console.log(message);
+    });
+
+    // connect the client
+    console.log("connecting...");
+    client
+      .connect()
+      .then(() => {
+        // Once a connection has been made, make a subscription and send a message.
+        console.log("onConnect");
+        return client.subscribe("$SYS/#");
+      })
+      .catch((responseObject: { errorCode: number; errorMessage: string }) => {
+        if (responseObject.errorCode !== 0) {
+          console.log("onConnectionLost:" + responseObject.errorMessage);
+        }
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +63,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
